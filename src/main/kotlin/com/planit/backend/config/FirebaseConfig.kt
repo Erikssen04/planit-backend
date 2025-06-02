3db6971c00c8f22c11dc66793b7285f55d4e3733
Erikssen04 <emacgom875@g.educaand.es>
2025-06-02 16:46:05 +0200

    backend listo para despliegue + variable de entorno

--- src/main/kotlin/com/planit/backend/config/FirebaseConfig.kt
+++ src/main/kotlin/com/planit/backend/config/FirebaseConfig.kt
@@ -4,19 +4,31 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
 import java.io.FileInputStream
 
 @Configuration
 class FirebaseConfig {
     init {
 
-        val credentialsPath = System.getenv("FIREBASE_CREDENTIALS_PATH")
+        /*val credentialsPath = System.getenv("FIREBASE_CREDENTIALS_PATH")
             ?: throw IllegalStateException("FIREBASE_CREDENTIALS_PATH environment variable is not set")
 
         val serviceAccount = FileInputStream(credentialsPath)
 
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .build()*/
+
+        val firebaseConfig = System.getenv("FIREBASE_CREDENTIALS_JSON")
+            ?: throw IllegalStateException("FIREBASE_CREDENTIALS_JSON environment variable is not set")
+
+        val credentials = GoogleCredentials.fromStream(
+            ByteArrayInputStream(firebaseConfig.toByteArray(Charsets.UTF_8))
+        )
+
+        val options = FirebaseOptions.builder()
+            .setCredentials(credentials)
             .build()
 
         if (FirebaseApp.getApps().isEmpty()) {
